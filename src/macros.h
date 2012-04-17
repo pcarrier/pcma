a8eda16f53fe8999f3f06679c93e6ee702a748a2
Pierre Carrier <pierre@spotify.com>
2012-04-17 02:53:11 +0200

    src/macros.h: remove indentation in logs

--- src/macros.h
+++ src/macros.h
@@ -2,11 +2,11 @@
 #define PCMA__MACROS_H
 
 #define LOG_SERV(...) \
-	fprintf(stderr, "[SERV]  " __VA_ARGS__);
+	fprintf(stderr, "[SERVER] " __VA_ARGS__);
 #define LOG_DEBUG(...) \
 	{if (log_level > 2) fprintf(stderr, "[DEBUG] " __VA_ARGS__);}
 #define LOG_INFO(...) \
-	{if (log_level > 1) fprintf(stderr, "[INFO]  " __VA_ARGS__);}
+	{if (log_level > 1) fprintf(stderr, "[INFO] " __VA_ARGS__);}
 #define LOG_ERROR(...) \
 	{if (log_level > 0) fprintf(stderr, "[ERROR] " __VA_ARGS__);}
 
